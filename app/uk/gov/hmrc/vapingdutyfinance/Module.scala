a80cc2156503fe88867fbebc2b39afc6b9f88a80
ghousedev <49005870+ghousedev@users.noreply.github.com>
2025-12-22 10:18:42 +0000

    VPD-1201: add action to module

--- app/uk/gov/hmrc/vapingdutyfinance/Module.scala
+++ app/uk/gov/hmrc/vapingdutyfinance/Module.scala
@@ -18,14 +18,17 @@ package uk.gov.hmrc.vapingdutyfinance
 
 import play.api.{Configuration, Environment}
 import play.api.inject.{Binding, Module => AppModule}
+import uk.gov.hmrc.vapingdutyfinance.controllers.actions.{AuthorisedAction, BaseAuthorisedAction}
 
 import java.time.Clock
 
-class Module extends AppModule:
+class Module extends AppModule {
 
   override def bindings(
-    environment  : Environment,
-    configuration: Configuration
-  ): Seq[Binding[_]] =
-    bind[Clock].toInstance(Clock.systemDefaultZone) :: // inject if current time needs to be controlled in unit tests
-    Nil
+                         environment: Environment,
+                         configuration: Configuration
+                       ): Seq[Binding[_]] =
+        bind[Clock].toInstance(Clock.systemDefaultZone) ::
+        bind[AuthorisedAction].to(classOf[BaseAuthorisedAction]) ::
+        Nil
+}
